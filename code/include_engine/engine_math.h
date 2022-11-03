7962703a65b6d11a29cf38b681a77d09d8d69cd3
juvus <juvus@yandex.ru>
2022-11-03 14:28:40 +0200

    Adding math functions to the engine.

--- code/include_engine/engine_math.h
+++ code/include_engine/engine_math.h
@@ -1,4 +1,29 @@
-/* Engine math */
+/**
+ * ================================================================================
+ * @file engine_math.h
+ * @author Dmitry Safonov (juvusoft@gmail.com)
+ * @brief Declaration of structures and functions necessary for performing 
+ * different calculations.
+ * @version 0.1
+ * @date 2022-11-02
+ * ================================================================================ 
+ */
+
+#ifndef ENGINE_MATH_H_
+#define ENGINE_MATH_H_
+
+/* Game engine includes: */
+#include "utils.h"
+
+/**
+ * @brief Determination if two f32 numbers are nearly equal.
+ * @param x First f32 number.
+ * @param y Second f32 number.
+ * @return b32 Result of the comparison. 
+ */
+b32
+is_nearly_equal(f32 x, f32 y);
+
 
 
 /* Vorking with vectors:  */
@@ -18,3 +43,12 @@
 
 */
 
+
+/*
+vector solve_equations_system(matrix, vector)
+
+*/
+
+
+
+#endif // ENGINE_MATH_H_
