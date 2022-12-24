fff6a1df6b198453a0058739fec5b68afa8a4439
juvus <juvus@yandex.ru>
2022-12-24 15:37:34 +0200

    Added functionality. Now matrix 3x3 is suported.

---
+++ code/include_engine/matrix33.h
@@ -0,0 +1,160 @@
+/**
+ * ================================================================================
+ * @file include_engine/matrix33.h
+ * @author Dmitry Safonov (juvusoft@gmail.com)
+ * @brief Declaration of structures and functions necessary for the work with 
+ * 3-by-3 matrix with elements of f32 (32 bits) floating point type.
+ * @version 0.2
+ * @date 2022-12-06
+ * ================================================================================ 
+ */
+
+#ifndef JGE_ENGINE_MATRIX33_H_
+#define JGE_ENGINE_MATRIX33_H_
+
+#include "include_engine/utils.h"
+
+typedef struct Vec3_ Vec3;
+
+/**
+ * @brief Structure to store the 3-by-3 matrix with elements of f32 type.
+ */
+struct Mat33_
+{
+    union
+    {
+        struct
+        {
+            f32 a11;  /**< Matrix 1st element of the 1st column. */
+            f32 a21;  /**< Matrix 2nd element of the 1st column. */ 
+            f32 a31;  /**< Matrix 3rd element of the 1st column. */
+            f32 a12;  /**< Matrix 1st element of the 2nd column. */
+            f32 a22;  /**< Matrix 2nd element of the 2nd column. */
+            f32 a32;  /**< Matrix 3rd element of the 2nd column. */
+            f32 a13;  /**< Matrix 1st element of the 3rd column. */
+            f32 a23;  /**< Matrix 2nd element of the 3rd column. */
+            f32 a33;  /**< Matrix 3rd element of the 3rd column. */
+        };
+        f32 elem_arr[9];  /* Array of the matrix elements. */
+    };
+};
+typedef struct Mat33_ Mat33;
+
+/**
+ * @brief Object constructor.
+ * @return Mat33* Pointer to the Mat33 structure.
+ */
+Mat33*
+Mat33_Constructor(void);
+
+/**
+ * @brief Object descructor.
+ * @param mat33 Pointer to the Mat33 structure.
+ */
+void
+Mat33_Destructor(Mat33 *mat33);
+
+/**
+ * @brief Initialization of the matrix by element values.
+ * @param mat33 Pointer to the Mat33 structure.
+ * @param a11 Matrix 1st element of the 1st column.
+ * @param a21 Matrix 2nd element of the 1st column.
+ * @param a31 Matrix 3rd element of the 1st column.
+ * @param a12 Matrix 1st element of the 2nd column.
+ * @param a22 Matrix 2nd element of the 2nd column.
+ * @param a32 Matrix 3rd element of the 2nd column.
+ * @param a13 Matrix 1st element of the 3rd column.
+ * @param a23 Matrix 2nd element of the 3rd column.
+ * @param a33 Matrix 3rd element of the 3rd column.
+ */
+void
+Mat33_InitByF32(Mat33 *mat33, f32 a11, f32 a21, f32 a31, f32 a12, f32 a22, f32 a32, 
+    f32 a13, f32 a23, f32 a33);
+
+/**
+ * @brief Initialization of the matrix by column vectors.
+ * @param mat33 Pointer to the Mat33 structure.
+ * @param col1 Pointer to the Vec3 structure (first column).
+ * @param col2 Pointer to the Vec3 structure (second column).
+ * @param col3 Pointer to the Vec3 structure (third column).
+ */
+void
+Mat33_InitByVec3(Mat33 *mat33, const Vec3 *col1, const Vec3 *col2, const Vec3 *col3);
+
+/**
+ * @brief Initialization of the matrix by data from the source matrix.
+ * @param mat33 Pointer to Mat33 structure (destination).
+ * @param mat33_src Pointer to Mat33 structure (source).
+ */
+void
+Mat33_InitByMat33(Mat33 *mat33, const Mat33 *mat33_src);
+
+/**
+ * @brief Determination if matrix contains finite elements. 
+ * @param mat33 Pointer to Mat33 structure.
+ * @return b32 True - valid matrix. False - not valid matrix.
+ */
+b32
+Mat33_IsValid(const Mat33 *mat33);
+
+/**
+ * @brief Determination if matrix is a nearly zero matrix.
+ * @param mat33 Pointer to Mat33 structure.
+ * @return b32 Result of the check.
+ */
+b32
+Mat33_IsNearlyZero(const Mat33 *mat33);
+
+/**
+ * @brief Set all matrix elements to zeros.
+ * @param mat33 Pointer to Mat33 structure.
+ */
+void
+Mat33_SetZero(Mat33 *mat33);
+
+/**
+ * @brief Set matrix to the identity matrix.
+ * @param mat33 Pointer to Mat33 structure.
+ */
+void
+Mat33_SetIdentity(Mat33 *mat33);
+
+/**
+ * @brief Multiply the matrix by a scalar.
+ * @param mat33 Pointer to Mat33 structure.
+ * @param scalar Arbitrary scalar.
+ */
+void
+Mat33_MultiplyScalar(Mat33 *mat33, f32 scalar);
+
+/**
+ * @brief Calculation of the 3-by-3 matrix determinant.
+ * @param mat33 Pointer to Mat33 structure.
+ * @return f32 Matrix determinant value. 
+ */
+f32
+Mat33_CalcDeterminant(const Mat33 *mat33);
+
+/**
+ * @brief Make transposition of the matrix.
+ * @param mat33 Pointer to Mat33 structure.
+ */
+void
+Mat33_MakeTranspose(Mat33 *mat33);
+
+/**
+ * @brief Make cofactor matrix from the given matrix.
+ * @param mat33 Pointer to Mat33 structure.
+ */
+void
+Mat33_MakeCofactor(Mat33 *mat33);
+
+/**
+ * @brief Make invesion of the matrix.
+ * @param mat33 Pointer to Mat33 structure.
+ * @return b32 Flag showing if the invesion was successful or not. 
+ */
+b32
+Mat22_MakeInverse(Mat33 *mat33);
+
+#endif  /* JGE_ENGINE_MATRIX33_H_ */
---
+++ code/src_engine/matrix33.c
@@ -0,0 +1,167 @@
+/**
+ * ================================================================================
+ * @file src_engine/matrix33.c
+ * @author Dmitry Safonov (juvusoft@gmail.com)
+ * @brief Definition of functions necessary for the work with 3-by-3 matrix with 
+ * elements of f32 (32 bits) floating point type.
+ * @version 0.2
+ * @date 2022-12-09
+ * ================================================================================
+ */
+
+#define _USE_MATH_DEFINES
+
+#include "include_engine/matrix33.h"
+
+#include <math.h>
+
+#include "include_engine/dbg.h"
+#include "include_engine/math_functions.h"
+#include "include_engine/utils.h"
+#include "include_engine/vector3.h"
+
+Mat33*
+Mat33_Constructor(void)
+{
+    Mat33 *mat33 = (Mat33 *)malloc(1 * sizeof(Mat33));
+    if (mat33 == NULL)
+    {
+        dbg_error("%s", "Memory allocation error!");
+    }
+    return mat33;
+}
+
+void
+Mat33_Destructor(Mat33 *mat33)
+{
+    if (mat33 == NULL)
+    {
+        dbg_error("%s", "Attempt to delete an empty object!");
+    }
+    free(mat33);
+    mat33 = NULL;
+}
+
+void
+Mat33_InitByF32(Mat33 *mat33, f32 a11, f32 a21, f32 a31, f32 a12, f32 a22, f32 a32,
+    f32 a13, f32 a23, f32 a33)
+{
+    mat33->a11 = a11; mat33->a12 = a12; mat33->a13 = a13;
+    mat33->a21 = a21; mat33->a22 = a22; mat33->a23 = a23;
+    mat33->a31 = a31; mat33->a32 = a32; mat33->a33 = a33;
+}
+
+void
+Mat33_InitByVec3(Mat33 *mat33, const Vec3 *col1, const Vec3 *col2, const Vec3 *col3)
+{
+    Mat33_InitByF32(mat33, col1->x, col1->y, col1->z, col2->x, col2->y, col2->z, 
+        col3->x, col3->y, col3->z);
+}
+
+void
+Mat33_InitByMat33(Mat33 *mat33, const Mat33 *mat33_src)
+{
+    *mat33 = *mat33_src;
+}
+
+b32
+Mat33_IsValid(const Mat33 *mat33)
+{
+    for (u32 i = 0; i < 9; ++i)
+    {
+        if (!isfinite(mat33->elem_arr[i])) return false;
+    }
+    return true;
+}
+
+b32
+Mat33_IsNearlyZero(const Mat33 *mat33)
+{
+    for (u32 i = 0; i < 9; ++i)
+    {
+        if (!Math_IsNearlyEqual(mat33->elem_arr[i], 0.0f)) return false;
+    }
+    return true;
+}
+
+void
+Mat33_SetZero(Mat33 *mat33)
+{
+    Mat33_InitByF32(mat33, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+}
+
+void
+Mat33_SetIdentity(Mat33 *mat33)
+{
+    Mat33_InitByF32(mat33, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void
+Mat33_MultiplyScalar(Mat33 *mat33, f32 scalar)
+{
+    for (u32 i = 0; i < 9; ++i)
+    {
+        mat33->elem_arr[i] *= scalar;
+    }
+}
+
+f32
+Mat33_CalcDeterminant(const Mat33 *mat33)
+{
+    f32 a = mat33->a11; f32 b = mat33->a12; f32 c = mat33->a13;
+    f32 d = mat33->a21; f32 e = mat33->a22; f32 f = mat33->a23;
+    f32 g = mat33->a31; f32 h = mat33->a32; f32 i = mat33->a33;
+
+    f32 det_mat22_a = e * i - f * h;
+    f32 det_mat22_b = d * i - f * g;
+    f32 det_mat22_c = d * h - e * g;
+
+    f32 det = a * det_mat22_a - b * det_mat22_b + c * det_mat22_c;
+    return det;
+}
+
+void
+Mat33_MakeCofactor(Mat33 *mat33)
+{
+    f32 a = mat33->a11; f32 b = mat33->a12; f32 c = mat33->a13;
+    f32 d = mat33->a21; f32 e = mat33->a22; f32 f = mat33->a23;
+    f32 g = mat33->a31; f32 h = mat33->a32; f32 i = mat33->a33;
+
+    /* Cofactor coefficients: c_ij = (-1)^(i+j) * m_ij. Term m_ij is a determinant of the 
+    minor matrix. */
+    f32 c11 = e * i - f * h; f32 c12 = -(d * i - f * g); f32 c13 = d * h - e * g;
+    f32 c21 = -(b * i - c * h); f32 c22 = a * i - c * g; f32 c23 = -(a * h - b * g);
+    f32 c31 = b * f - c * e; f32 c32 = -(a * f - c * d); f32 c33 = a * e - b * d;    
+    
+    Mat33_InitByF32(mat33, c11, c21, c31, c12, c22, c32, c13, c23, c33);
+}
+
+void
+Mat33_MakeTranspose(Mat33 *mat33)
+{
+    f32 a = mat33->a11; f32 b = mat33->a12; f32 c = mat33->a13;
+    f32 d = mat33->a21; f32 e = mat33->a22; f32 f = mat33->a23;
+    f32 g = mat33->a31; f32 h = mat33->a32; f32 i = mat33->a33;
+    
+    Mat33_InitByF32(mat33, a, b, c, d, e, f, g, h, i);
+}
+
+b32
+Mat33_MakeInverse(Mat33 *mat33)
+{
+    f32 det = Mat33_CalcDeterminant(mat33);
+    if (Math_IsNearlyEqual(det, 0.0f))
+    {
+        /* Not possible to get the inverse matrix. */
+        return false;
+    }
+    else
+    {
+        /* Inverse matrix: M^(-1) = 1/det(M) * adj(A) = 1/det(M) * cof(A)^(T). */
+        f32 det_rev = 1.0f / det;
+        Mat33_MakeCofactor(mat33);
+        Mat33_MakeTranspose(mat33);
+        Mat33_MultiplyScalar(mat33, det_rev);
+        return true;
+    }
+}
